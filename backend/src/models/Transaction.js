94a382cc5a5a2323ae18ef67cb216ba68de52134
Anushka Dutta <anushka.23FE10CSE00399@muj.manipal.edu>
2026-06-03 18:07:10 +0530

    Create transaction intelligence schema

---
+++ backend/src/models/Transaction.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    amount: {
+      type: Number,
+      required: true,
+    },
+
+    receiverUpiId: {
+      type: String,
+      required: true,
+    },
+
+    note: {
+      type: String,
+      default: "",
+    },
+
+    riskScore: {
+      type: Number,
+      default: 0,
+    },
+
+    riskLevel: {
+      type: String,
+      default: "LOW",
+    },
+
+    riskReasons: [
+      {
+        type: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Transaction", transactionSchema);
